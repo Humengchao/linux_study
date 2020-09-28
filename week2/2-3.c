cfcad4c58f2109b509b25ddd80ba8fb931020b4f
Humengchao <810244965@qq.com>
2020-09-28 14:33:47 +0800

    week2 code

---
+++ week2/2-3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main(){
+	int ret;
+	ret = write(0, "hello world!", 13);
+	printf("ret=%d\n", ret);
+}
---
+++ week2/2-4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main(){
+	int ret;
+	ret = write(2, "hello world!\n", 13);
+	printf("ret=%d\n", ret);
+	sleep(500);
+}
